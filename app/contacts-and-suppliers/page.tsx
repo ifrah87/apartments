163cf955440b37984504211469d5a15c5b69c74f
ifrah87 <ifrahawaale@gmail.com>
2026-02-15 14:45:25 +0300

    Fix contacts-and-suppliers page export

---
+++ app/contacts-and-suppliers/page.tsx
@@ -0,0 +1,10 @@
+export default function ContactsAndSuppliersPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1 style={{ fontSize: 24, fontWeight: 700 }}>Contacts & Suppliers</h1>
+      <p style={{ marginTop: 8, opacity: 0.8 }}>
+        Page placeholder. (We’ll wire this up after deploy.)
+      </p>
+    </main>
+  );
+}
