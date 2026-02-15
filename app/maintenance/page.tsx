87b3d86f74b5b155ab089405afd6898ccc93febc
ifrah87 <ifrahawaale@gmail.com>
2026-02-15 16:17:36 +0300

    Fix maintenance page export

---
+++ app/maintenance/page.tsx
@@ -0,0 +1,8 @@
+export default function MaintenancePage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1 style={{ fontSize: 24, fontWeight: 700 }}>Maintenance</h1>
+      <p style={{ marginTop: 8 }}>Coming soon.</p>
+    </main>
+  );
+}
