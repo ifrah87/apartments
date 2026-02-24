74c102d291fb3759a920941c3564699d94df1cdf
ifrah87 <ifrahawaale@gmail.com>
2026-02-24 22:47:22 +0300

    fix: make properties unique code migration idempotent

--- db/migrations/048_properties_unique_code.sql
+++ db/migrations/048_properties_unique_code.sql
@@ -1,18 +1,7 @@
 ALTER TABLE public.properties
 ADD COLUMN IF NOT EXISTS code text;
 
-DO $$
-BEGIN
-  IF EXISTS (
-    SELECT 1
-    FROM pg_indexes
-    WHERE schemaname = 'public'
-      AND tablename = 'properties'
-      AND indexname = 'uq_properties_code'
-  ) THEN
-    EXECUTE 'ALTER INDEX public.uq_properties_code RENAME TO properties_code_unique';
-  END IF;
-END$$;
+DROP INDEX IF EXISTS public.uq_properties_code;
 
 CREATE UNIQUE INDEX IF NOT EXISTS properties_code_unique
 ON public.properties (code)
